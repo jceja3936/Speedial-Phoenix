1488dd822a72a383326f3e415e811fc93acb61b7
Jesse Ceja <jceja3936@gmail.com>
2025-05-24 14:33:32 -0700

    Name Change

--- speedialChicago/scenes/hun.gd
+++ speedialChicago/scenes/hun.gd
@@ -9,13 +9,5 @@ func update_values(value: int):
 	match value:
 		1:
 			currentSprite = load("res://assets/basicSquare.svg")
-	update_Self()
-
-func update_Self():
 	if (currentSprite != null):
 		texture = currentSprite
-
-func getAmmo():
-	return ammo
-func getMags():
-	return mags
