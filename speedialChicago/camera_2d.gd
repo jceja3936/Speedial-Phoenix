d267a6efb7060e1f7e1cea5a4385abaee3267abd
Jesse Ceja <jceja3936@gmail.com>
2025-06-20 14:07:18 -0700

    Cursor Added And Cam fix
    The camera is no longer horrible, and a cursor was added that orbits the player so you know where you're aiming

--- speedialChicago/camera_2d.gd
+++ speedialChicago/camera_2d.gd
@@ -5,8 +5,14 @@ var cameFollow = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if cameFollow:
-		position = lerp(player.global_position, get_global_mouse_position(), .5);
+	if cameFollow and player.get_global_position().distance_to(get_global_mouse_position()) > 290:
+		position = lerp(player.get_global_position(), (player.get_global_position() + Vector2(500, 0).rotated(get_angle_to(get_global_mouse_position()))), .1)
+		$Cursors.position = player.get_global_position()
+		$Cursors.offset = Vector2(500, 0).rotated(get_angle_to(get_global_mouse_position()))
+	else:
+		position = lerp(position, player.get_global_position(), 1)
+		$Cursors.position = get_global_mouse_position()
+		$Cursors.offset = Vector2.ZERO
 
 	if Input.is_action_just_pressed("Respawn") and player.get("dead") == null:
 		get_tree().change_scene_to_file("res://scenes/level.tscn")
@@ -21,10 +27,12 @@ func _process(_delta: float) -> void:
 
 func updateAmmo(amount: int):
 	$AmAm.show()
+	$AmAm.top_level = true
 	$AmAm.text = "Ammo: " + str(amount)
 
 
 func _ready() -> void:
+	$Cursors.top_level = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED_HIDDEN)
 	$AmAm.hide()
 	$Respawn.hide()
