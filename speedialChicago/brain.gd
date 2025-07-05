074e560c84a76f632fb5ed3f0603f1080c277f3f
Jesse Ceja <jceja3936@gmail.com>
2025-07-05 13:08:06 -0700

    Start and end trigger zones
    Trigger boxes made more useful

--- speedialChicago/brain.gd
+++ speedialChicago/brain.gd
@@ -1,6 +1,6 @@
 extends Node
 @export var player: CharacterBody2D
-var warpPosition = Vector2.ZERO
+var endPosition = Vector2.ZERO
 var state = 1
 var levelBeat = false
 
@@ -13,31 +13,54 @@ func setState(newState: int):
 	match state:
 		1:
 			Manager.setEnemyAmount(3)
-			warpPosition = Vector2(-57.0, 1023)
+			endPosition = Vector2(-57.0, 1023)
+			$start.position = Vector2(1097.0, 328.0)
 		2:
 			Manager.setEnemyAmount(4)
-			warpPosition = Vector2(2214.0, 2677.0)
+			endPosition = Vector2(2214.0, 2677.0)
+			$start.position = Vector2(1720.0, 2715.0)
+		3:
+			endPosition = Vector2(-57.0, 1023)
+			$start.position = Vector2(1097.0, 328.0)
 		_:
 			print("Bruh")
 
 	if levelBeat == true:
 		Manager.setEnemyAmount(0)
+		$start.position = Vector2(1097.0, 328.0)
 
-	$Area2D/CollisionShape2D.position = warpPosition
+	$end.position = endPosition
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+
+func _on_start_body_entered(body: Node2D) -> void:
+	if Manager.levelState == 3:
+		get_tree().quit()
+
+	if body.name == "Player":
+		match state:
+				1:
+					$blocker.position = Vector2(1107.0, 207.0)
+				2:
+					$blocker.position = Vector2(2079, 2598.0)
+	if Manager.getEnemyAmount() == 0:
+		$blocker.position = Vector2.ZERO
+					
+
+func _on_end_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		if Manager.getEnemyAmount() == 0:
 			match state:
 				1:
 					player.makeHunSave()
-					Manager.playerRespawnPos = Vector2(2121.0, 2606.0)
+					Manager.playerRespawnPos = Vector2(1839.0, 2611.0)
 					player.position = Vector2(2121.0, 2606.0)
 					Manager.levelState = 2
 					setState(2)
 				2:
 					levelBeat = true
 					player.position = Vector2(124.0, 1038.0)
-					Manager.playerRespawnPos = Vector2(124.0, 1038.0)
 					Manager.levelState = 3
-					setState(1)
+					setState(3)
+				3:
+					player.position = Vector2(2121.0, 2606.0)
+					setState(2)
