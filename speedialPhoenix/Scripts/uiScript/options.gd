1ebc35bdf985d291f67d761f79b35b17c45fbcc1
Jesse Ceja <44848371+jceja3936@users.noreply.github.com>
2025-08-08 19:12:21 -0700

    Controller controls have been added to options
    Small change but game should be ready for live

--- speedialPhoenix/Scripts/uiScript/options.gd
+++ speedialPhoenix/Scripts/uiScript/options.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _ready():
+	$slide.mouse_filter = Control.MOUSE_FILTER_STOP
 	$exit.grab_focus()
 
 func _on_sound_drag_ended(_value_changed: bool) -> void:
@@ -13,3 +14,23 @@ func _on_music_drag_ended(_value_changed: bool) -> void:
 func _on_exit_pressed() -> void:
 	Manager.next_scene = "res://scenes/UIscenes/start_menu.tscn"
 	Manager.startNextScene()
+
+
+func _on_slide_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$Controlls.text = "Controls:
+		Movement                  Left Stick
+		Camera                      Right Stick
+		Pick Up/Drop             L1
+		Fire                             R2
+		Finish                          X
+		Look Far                     L2"
+	else:
+		$Controlls.text = "Controls:
+		Movement                  WASD
+		Camera                      Mouse
+		Pick Up/Drop             F
+		Fire                             LMB
+		Finish                          Space
+		Look Far                     RMB (Hold)
+		"
